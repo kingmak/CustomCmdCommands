5354a38674c7c284ffb0db87292dd6699ab6a903
kingmak <kingmak@hotmail.com>
2016-08-22 05:16:32 -0500

    Create minn.cpp

---
+++ minimize/minn.cpp
@@ -0,0 +1,48 @@
+#include <Windows.h>
+
+/*
+To disable console window: 
+
+Tools-> Compiler Options-> Settings-> Linker
+then change the fourth box to yes.
+
+But you have to change it back for you're normal console apps.
+
+////////////////////////////////////////////////////////////////////
+
+name of this file = cmd command
+need a batch script to run command
+
+////////////////////////////////////////////////////////////////////
+
+batch code:
+@echo off
+start minn.exe
+
+////////////////////////////////////////////////////////////////////
+command is min
+minn.exe is placed in C:\Windows\System32\
+can do where min in cmd
+*/
+ 
+int main()
+{
+	LPCSTR windowNames[4] = {
+								"Command Prompt",
+								"C:\\Windows\\system32\\cmd.exe",
+								"Select Command Prompt",
+								"Select C:\\Windows\\system32\\cmd.exe"
+							};  // arranged my most likely
+							
+							
+	for (int i = 0; i < 4; i++)
+	{
+		if (FindWindow(0, windowNames[i])) // if window found [return type of FindWindow is HWND (window handle)]
+		{
+			ShowWindow(FindWindow(0, windowNames[i]), SW_MINIMIZE); // minimize the window
+			return 0; // exit main
+		}
+	}
+	
+	return 0; // window not found, exit :(
+}	
